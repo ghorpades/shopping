9e6748ca8e3ae64dcf0e73a7c679e4b5aa9a847d
sanjeev <sanjeev.ghorpade@gmail.com>
2018-04-10 14:42:06 +0530

    My Project Setup completed sucessfully

---
+++ shopping-backend/src/main/java/com/avaya/shopping_backend/App.java
@@ -0,0 +1,13 @@
+package com.avaya.shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
